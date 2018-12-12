0e136ed0830f383132701d14d0963133fac4df98
jad <chehimj@gmail.com>
2018-12-11 21:12:41 -0700

    v3.0.0

--- script.js
+++ script.js
@@ -1,63 +1,54 @@
+const version = "3.0.0";
+
 let now = moment();
 
 const timeFormat = "h:mm:ss a";
-const dateFormat = "MM-DD";
 
 const clock = document.getElementById("clock");
-let visible = false;
-
-let target;
+let active = true;
 
 (() => {
-  blockHandler();
-  setClock();
-  addAnimation();
+  document.addEventListener("mouseup", onInteract, false);
+  document.addEventListener("touchend", onInteract, false);
+
+  // Version display
+  document.getElementById("version").innerHTML = "Version: " + version;
+  setTimeout(() => {
+    document.getElementById("version").style.display = "none";
+  }, 4000);
 })();
 
-function setClock(){
+// Animation handler
+function onInteract(){
 
-  // Touch detection
-  document.addEventListener("mouseup", addAnimation, false);
-  document.addEventListener("touchend", addAnimation, false);
-
-  // Animation listener
-  clock.addEventListener("animationend", () => { clock.className = ""; visible = false; },false);
-
-  // Styling
-  if(isCCD()) clock.style.color = "#ffff77";
+  active = true;
   updateClock();
+  clock.className = "animation";
+
+  if(isCCD()) clock.style.color = "#ffff77";
+
+  clock.addEventListener("animationend", () => {
+
+    active = false;
+    clock.className = "";
+    updateClock();
+
+  }, false);
 }
 
 function updateClock(){
   // SET TIME
   now = moment();
 
-  if(visible){
-    // Setting time
-    clock.innerHTML = target;
-
-    // Set correct target time
-    blockHandler();
-  }
-
-  // Loop on interval
-  setTimeout(updateClock, 1000); 
+  clock.innerHTML = timeUntilNextBlock();
+  if(active) setTimeout(updateClock, 100);
 }
 
-function addAnimation(e){
-  if(e != undefined) e.preventDefault();
-
-  if(!visible){
-    blockHandler();
-    visible = true;
-    clock.className += " animation";
-  }
-}
-
+// Date checkers
 function isCCD() {
-  let today = now.format(dateFormat);
-  let ccdDay = now.endOf("month").day("Thursday").format(dateFormat);
-  return (today == ccdDay) ? true : false;
+  let ccdDay = moment(now).endOf("month");
+  while(ccdDay.day() !== 4) ccdDay.subtract(1, 'day');
+  return (now.format("DD/MM/YYYY") == ccdDay.format("DD/MM/YYYY")) ? true : false;
 }
 
 function isMonday() {
@@ -68,17 +59,12 @@ function isWeekend(){
   return (now.isoWeekday() == 6 || now.isoWeekday() == 7) ? true : false;
 }
 
-// stackoverflow
-function pad(string) {
-  var sign = Math.sign(string) === -1 ? '-' : '';
-  return sign + new Array(2).concat([Math.abs(string)]).join('0').slice(-2);
-}
-
+// Time Checkers
 function timeUntil(date){
   let dur = moment.utc(date.diff(now));
-  let format = "ss";
+  let format = "s";
 
-  if(dur.minute() > 0) format = "mm:ss";
+  if(dur.minute() > 0) format = "m:ss";
   if(dur.hour() > 0) format = "h:mm:ss";
 
   return dur.format(format);
@@ -88,34 +74,31 @@ function between(before, after){
   return now.isBetween(moment(before, timeFormat), moment(after, timeFormat))
 }
 
-function blockHandler(){
-  target = undefined;
+// Block checkers
+function timeUntilNextBlock(){
 
-  if(isWeekend()){
-    target = "No school today";
-    return;
-  }
+  if(isWeekend()) return "No school today";
 
   if(isMonday()){
-    if(between("9:00 am", "10:05 am")) target = timeUntil(moment("10:05 am", timeFormat));
-    if(between("10:05 am", "11:15 am")) target = timeUntil(moment("11:15 am", timeFormat));
-    if(between("11:15 am", "12:28 pm")) target = timeUntil(moment("12:28 pm", timeFormat));
-    if(between("12:28 pm", "1:13 pm")) target = timeUntil(moment("1:13 pm", timeFormat));
-    if(between("1:13 pm", "2:18 pm")) target = timeUntil(moment("2:18 pm", timeFormat));
-    if(between("2:18 pm", "3:30 pm")) target = timeUntil(moment("3:30 pm", timeFormat));
+    if( between("9:00 am", "10:05 am")  )   return timeUntil(moment("10:05 am", timeFormat));
+    if( between("10:05 am", "11:15 am") )   return timeUntil(moment("11:15 am", timeFormat));
+    if( between("11:15 am", "12:28 pm") )   return timeUntil(moment("12:28 pm", timeFormat));
+    if( between("12:28 pm", "1:13 pm")  )   return timeUntil(moment("1:13 pm", timeFormat));
+    if( between("1:13 pm", "2:18 pm")   )   return timeUntil(moment("2:18 pm", timeFormat));
+    if( between("2:18 pm", "3:30 pm")   )   return timeUntil(moment("3:30 pm", timeFormat));
   }else if(isCCD()){
-    if(between("9:00 am", "9:53 am")) target = timeUntil(moment("9:53 am", timeFormat));
-    if(between("9:53 am", "10:53 am")) target = timeUntil(moment("10:53 am", timeFormat));
-    if(between("10:53 am", "11:53 am")) target = timeUntil(moment("11:53 am", timeFormat));
-    if(between("11:53 am", "12:53 pm")) target = timeUntil(moment("12:53 pm", timeFormat));
+    if( between("9:00 am", "9:53 am")   )   return timeUntil(moment("9:53 am", timeFormat));
+    if( between("9:53 am", "10:53 am")  )   return timeUntil(moment("10:53 am", timeFormat));
+    if( between("10:53 am", "11:53 am") )   return timeUntil(moment("11:53 am", timeFormat));
+    if( between("11:53 am", "12:53 pm") )   return timeUntil(moment("12:53 pm", timeFormat));
   }else{
-    if(between("9:00 am", "10:22 am")) target = timeUntil(moment("10:22 am", timeFormat));
-    if(between("10:22 am", "11:56 am")) target = timeUntil(moment("11:56 am", timeFormat));
-    if(between("11:56 am", "12:39 pm")) target = timeUntil(moment("12:39 pm", timeFormat));
-    if(between("12:39 pm", "2:01 pm")) target = timeUntil(moment("2:01 pm", timeFormat));
-    if(between("2:01 pm", "3:30 pm")) target = timeUntil(moment("3:30 pm", timeFormat));
+    if( between("9:00 am", "10:22 am")  )   return timeUntil(moment("10:22 am", timeFormat));
+    if( between("10:22 am", "11:56 am") )   return timeUntil(moment("11:56 am", timeFormat));
+    if( between("11:56 am", "12:39 pm") )   return timeUntil(moment("12:39 pm", timeFormat));
+    if( between("12:39 pm", "2:01 pm")  )   return timeUntil(moment("2:01 pm", timeFormat));
+    if( between("2:01 pm", "3:30 pm")   )   return timeUntil(moment("3:30 pm", timeFormat));
   }
 
   // Fallback
-  if(target == undefined) target = "After school";
+  return "After school";
 }
